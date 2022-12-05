4ddc3cff357f7e66a5f3eff89c14a66281499961
Dominik süßenbach <dominik.suessenbach@compleet.com>
2022-12-05 17:42:38 +0100

    Change bot name

--- config/teamspeak.php
+++ config/teamspeak.php
@@ -1,12 +1,14 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
     'ip' => env('TEAMSPEAK_IP', ''),
     'port' => env('TEAMSPEAK_PORT', 9987),
     'query_user' => env('TEAMSPEAK_QUERY_USER', ''),
     'query_password' => env('TEAMSPEAK_QUERY_PASSWORD', ''),
     'query_port' => env('TEAMSPEAK_QUERY_PORT', 10011),
-    'bot_name' => env('TEAMSPEAK_BOT_NAME', 'game-bot'),
+    'bot_name' => 'game-bot_'.Str::random(4),
 
     'auto_update_interval' => env('AUTO_UPDATE_INTERVAL', 1800),
 ];
